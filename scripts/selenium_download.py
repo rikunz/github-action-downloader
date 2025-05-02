993cfed6430ddf2d8b51d2971a8abc5d03304b1a
rikunz <thoriqfirdaus8@gmail.com>
2025-05-02 13:48:50 +0700

    t

--- scripts/selenium_download.py
+++ scripts/selenium_download.py
@@ -79,7 +79,6 @@ def trigger_1fichier_download(url):
         chrome_options.page_load_strategy = 'eager'
         chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("--disable-dev-shm-usage")
-        chrome_options.add_argument("--headless")
         chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36")
         chrome_options.add_argument("--disable-blink-features=AutomationControlled")
         chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
